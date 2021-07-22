c08ae52cac038ff2a127dd8d964460aa2b8c176f
DariusPirvulescu <organicdarius@gmail.com>
2021-07-22 19:44:52 +0300

    Calculate path for serve command files

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,11 +1,12 @@
 import { Command } from 'commander'
-import { serve } from '../../../local-api/src/index'
+import path from 'path'
+import {serve} from 'local-api'
 
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open file for editing')
   .option('-p, --port <number>', 'specify the port where to run the server', '4005')
   .action((filename='notebook.js', options: { port: string }) => {
-    console.log(filename, options)
-    serve(parseInt(options.port), filename, '/')
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename), dir)
   })
