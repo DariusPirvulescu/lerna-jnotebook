abc76c83442669c094c3ce83494a967d55551100
DariusPirvulescu <organicdarius@gmail.com>
2021-07-22 19:51:59 +0300

    Format code

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,12 +1,16 @@
-import { Command } from 'commander'
-import path from 'path'
-import {serve} from 'local-api'
+import { Command } from "commander";
+import path from "path";
+import { serve } from "local-api";
 
 export const serveCommand = new Command()
-  .command('serve [filename]')
-  .description('Open file for editing')
-  .option('-p, --port <number>', 'specify the port where to run the server', '4005')
-  .action((filename='notebook.js', options: { port: string }) => {
+  .command("serve [filename]")
+  .description("Open file for editing")
+  .option(
+    "-p, --port <number>",
+    "specify the port where to run the server",
+    "4005"
+  )
+  .action((filename = "notebook.js", options: { port: string }) => {
     const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), path.basename(filename), dir)
-  })
+    serve(parseInt(options.port), path.basename(filename), dir);
+  });
