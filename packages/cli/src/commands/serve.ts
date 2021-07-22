9ea9b5798c9d05444f893145db16d7c0fadcaedc
DariusPirvulescu <organicdarius@gmail.com>
2021-07-22 20:09:30 +0300

    Install dependencies for local-api, configure express server for serve command, handle command errors

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -10,7 +10,12 @@ export const serveCommand = new Command()
     "specify the port where to run the server",
     "4005"
   )
-  .action((filename = "notebook.js", options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      
+    } catch (error) {
+      console.log(error.message)
+    }
   });
