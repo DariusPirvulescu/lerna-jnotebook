f5e5f372a36f9831e34155c453d88db82c6ed152
DariusPirvulescu <organicdarius@gmail.com>
2021-07-27 00:15:15 +0300

    Check if production environment, configure serve function for different environment

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -2,6 +2,8 @@ import { Command } from "commander";
 import path from "path";
 import { serve } from "local-api";
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open file for editing")
@@ -13,7 +15,7 @@ export const serveCommand = new Command()
   .action(async (filename = "notebook.js", options: { port: string }) => {
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
-      await serve(parseInt(options.port), path.basename(filename), dir);
+      await serve(parseInt(options.port), path.basename(filename), dir, !isProduction);
       console.log(
         `${filename} is served. Navigate to http://localhost:${options.port} to edit the file.`
       );
