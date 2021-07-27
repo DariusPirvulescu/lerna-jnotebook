a5a8400cfdee734ade991d82a9ffd7d875de79a2
DariusPirvulescu <organicdarius@gmail.com>
2021-07-27 22:13:44 +0300

    Add cells router on local-api

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import path from 'path'
+import { createCellsRouter } from "./Routes/cells";
+
 
 export const serve = (port: number, filename: string, dir: string, isProxy: boolean) => {
   const app = express();
@@ -15,6 +17,8 @@ export const serve = (port: number, filename: string, dir: string, isProxy: bool
   } else {
     app.use(express.static(path.dirname(packagePath)))
   }
+
+  app.use(createCellsRouter(filename, dir))
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject)
